7f529dda8a6da0130f5a59e2c238b6afb98cb0d4
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2025-04-20 15:45:40 -0300

    Add files via upload

---
+++ cores.py
@@ -0,0 +1,14 @@
+#-------------------Cores-------------------------
+cor0 = "#2e2d2b" # preto
+cor1 = "#feffff" # branco
+cor2 = "#4fa882" # verde/azul claro
+cor3 = "#38576b" # Azul meio escuro
+cor4 = "#403d3d" # Preto
+cor5 = "#e06636" # Laranja
+cor6 = "#038cfc" # Azul claro
+cor7 = "#3fbfb9" # azul + claro
+cor8 = "#263238" # azul bem escuro
+cor9 = "#e9edf5" # branco
+#--------------------------------------------
+
+cores = ['#5588bb', '#66bbbb', '#99bb55', '#ee9944', '#444466','#bb5555'] #???????
