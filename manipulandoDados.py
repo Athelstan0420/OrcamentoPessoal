05badec0803cab6cc3c51fb11e42439ec308bddf
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2025-04-20 05:09:17 -0300

    Update manipulandoDados.py

--- manipulandoDados.py
+++ manipulandoDados.py
@@ -7,95 +7,90 @@ try:
 #--------------------------------------------------------------------------------------------------------
 
     #Estabelecendo conexão com o BD:
-    conexao = lite.connect("/home/adriel/Documentos/Develop/OrcamentoPessoal/dados.db")
+    conexaoBD = lite.connect("/home/adriel/Documentos/Develop/OrcamentoPessoal/dados.db")
     # Cursor que manipulará as tabela:
-    cursor = conexao.cursor()
+    cursorBD = conexaoBD.cursor()
 
  #--------------------------------------------------------------------------------------------------------
-
-    #Inserindo:
-
+#Inserindo:
 #--------------------------------------------------------------------------------------------------------
 
-    def inserir_Cateorias():
-        with conexao:
+    def inserir_Categorias():
+        with conexaoBD:
             consulta = "INSERT INTO Categorias (nome) VALUES (?)"
-            cursor.execute(consulta, ["Compras"])
+            cursorBD.execute(consulta, ["Compras"])
             #conexao.commit() #Para enviar dados a tabela. 
     # inserir_Cateorias()
     
     def inserir_Receitas(): 
-        with conexao:
+        with conexaoBD:
             consulta = "INSERT INTO Receitas (categoria, adicionado_em, valor) VALUES (?, ?, ?)"
-            cursor.execute(consulta, ["teste1", "12/03/2020", "100"])
+            cursorBD.execute(consulta, ["teste1", "12/03/2020", "100"])
             # conexao.commit() #Para enviar dados a tabela. 
     # inserir_Receitas()
     
     def inserir_Gastos():
-        with conexao:
+        with conexaoBD:
             consulta = "INSERT INTO Gastos (categoria, retirado_em, valor) VALUES (?, ?, ?)"
-            cursor.execute(consulta, ["teste3", "31/03/2015", "50"])
+            cursorBD.execute(consulta, ["teste3", "31/03/2015", "50"])
             #conexao.commit() #Para enviar dados a tabela. 
     # inserir_Gastos()
 #--------------------------------------------------------------------------------------------------------
-
-    # Deletando:
-
+# Deletando:
 #--------------------------------------------------------------------------------------------------------
 
     def deletar_Categorias():
-        with conexao:
+        with conexaoBD:
             consulta = "DELETE FROM Categorias WHERE id=?"
-            cursor.execute(consulta, "1")
+            cursorBD.execute(consulta, "1")
     # deletar_Categorias()
     
     def deletar_Receitas():
-        with conexao:
+        with conexaoBD:
             consulta = "DELETE FROM Receitas WHERE id=?"
-            cursor.execute(consulta, "1")
+            cursorBD.execute(consulta, "1")
     # deletar_Receitas()
 
     def deletar_Gastos():
-        with conexao:
+        with conexaoBD:
             consulta = "DELETE FROM Gastos WHERE id=?"
-            cursor.execute(consulta, "1")
+            cursorBD.execute(consulta, "1")
     # deletar_Gastos()
 
 #--------------------------------------------------------------------------------------------------------
-
-    # Ver categorias
-
+# Ver categorias
 #--------------------------------------------------------------------------------------------------------
 
-    tabelas()
+    # tabelas()
 
-    # lista_itens = []
-    # with conexao:
-    #     cursor.execute("SELECT * FROM Categorias")
-    #     linha = cursor.fetchall()
-    #     for lin in linha:
-    #         lista_itens.append(lin)
-    # print(lista_itens)
+    def vizualizar_categorias():
+        lista_itens = []
+        with conexaoBD:
+            cursorBD.execute("SELECT * FROM Categorias")
+            linha = cursorBD.fetchall()
+            for lin in linha:
+                lista_itens.append(lin)
+        # print(lista_itens)
 
-    # # Ver Receitas
+    # Ver Receitas
+    def vizu_receitas():
+        lista_itens = []
+        with conexaoBD:
+            cursorBD.execute("SELECT * FROM Receitas")
+            linha = cursorBD.fetchall()
+            for lin in linha:
+                lista_itens.append(lin)
+        # print(lista_itens)
 
-    # lista_itens = []
-    # with conexao:
-    #     cursor.execute("SELECT * FROM Receitas")
-    #     linha = cursor.fetchall()
-    #     for lin in linha:
-    #         lista_itens.append(lin)
-    # print(lista_itens)
-
-    # # Ver Gastos
-
-    # lista_itens = []
-    # with conexao:
-    #     cursor.execute("SELECT * FROM Gastos")
-    #     linha = cursor.fetchall()
-    #     for lin in linha:
-    #         lista_itens.append(lin)
-    # print(lista_itens)
+    # Ver Gastos
+    def vizu_gastos():
+        lista_itens = []
+        with conexaoBD:
+            cursorBD.execute("SELECT * FROM Gastos")
+            linha = cursorBD.fetchall()
+            for lin in linha:
+                lista_itens.append(lin)
+        # print(lista_itens)
 
 
 #--------------------------------------------------------------------------------------------------------
@@ -107,15 +102,12 @@ try:
 
 #--------------------------------------------------------------------------------------------------------
 
+    # def tabela2_manipuldados():
+    #     vizu_gastos = ''
 
 #--------------------------------------------------------------------------------------------------------
 
-
 #--------------------------------------------------------------------------------------------------------
-
-
-
-
 
 except:
     print("ERRO!")
